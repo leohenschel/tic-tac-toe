9514d45b70aee851b826e33437d51f1224e713fb
LEONARDO ALVES HENSCHEL <leonardo.ahenschel@senacsp.edu.br>
2019-06-15 16:11:02 -0300

    Jogo da velha SENAC

---
+++ jogo-da-velha/jogo-da-velha.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class JogodaVelha
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(JogodaVelha))
+        Me.BtnA1 = New System.Windows.Forms.Button()
+        Me.BtnB1 = New System.Windows.Forms.Button()
+        Me.BtnC1 = New System.Windows.Forms.Button()
+        Me.BtnA2 = New System.Windows.Forms.Button()
+        Me.BtnB2 = New System.Windows.Forms.Button()
+        Me.BtnC2 = New System.Windows.Forms.Button()
+        Me.BtnA3 = New System.Windows.Forms.Button()
+        Me.BtnB3 = New System.Windows.Forms.Button()
+        Me.BtnC3 = New System.Windows.Forms.Button()
+        Me.BtnNovoJogo = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LblJogador = New System.Windows.Forms.Label()
+        Me.BtnEditar = New System.Windows.Forms.Button()
+        Me.Lbl1 = New System.Windows.Forms.Label()
+        Me.LblVitoriasPlayer1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.LblVitoriasPlayer2 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.LblEmpates = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnA1
+        '
+        Me.BtnA1.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnA1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnA1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnA1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnA1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnA1.Location = New System.Drawing.Point(13, 153)
+        Me.BtnA1.Name = "BtnA1"
+        Me.BtnA1.Size = New System.Drawing.Size(58, 54)
+        Me.BtnA1.TabIndex = 0
+        Me.BtnA1.UseVisualStyleBackColor = False
+        '
+        'BtnB1
+        '
+        Me.BtnB1.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnB1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnB1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnB1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnB1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnB1.Location = New System.Drawing.Point(99, 153)
+        Me.BtnB1.Name = "BtnB1"
+        Me.BtnB1.Size = New System.Drawing.Size(58, 54)
+        Me.BtnB1.TabIndex = 1
+        Me.BtnB1.UseVisualStyleBackColor = False
+        '
+        'BtnC1
+        '
+        Me.BtnC1.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnC1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnC1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnC1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnC1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnC1.Location = New System.Drawing.Point(187, 153)
+        Me.BtnC1.Name = "BtnC1"
+        Me.BtnC1.Size = New System.Drawing.Size(58, 54)
+        Me.BtnC1.TabIndex = 2
+        Me.BtnC1.UseVisualStyleBackColor = False
+        '
+        'BtnA2
+        '
+        Me.BtnA2.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnA2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnA2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnA2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnA2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnA2.Location = New System.Drawing.Point(13, 240)
+        Me.BtnA2.Name = "BtnA2"
+        Me.BtnA2.Size = New System.Drawing.Size(58, 54)
+        Me.BtnA2.TabIndex = 3
+        Me.BtnA2.UseVisualStyleBackColor = False
+        '
+        'BtnB2
+        '
+        Me.BtnB2.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnB2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnB2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnB2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnB2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnB2.Location = New System.Drawing.Point(99, 240)
+        Me.BtnB2.Name = "BtnB2"
+        Me.BtnB2.Size = New System.Drawing.Size(58, 54)
+        Me.BtnB2.TabIndex = 4
+        Me.BtnB2.UseVisualStyleBackColor = False
+        '
+        'BtnC2
+        '
+        Me.BtnC2.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnC2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnC2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnC2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnC2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnC2.Location = New System.Drawing.Point(187, 240)
+        Me.BtnC2.Name = "BtnC2"
+        Me.BtnC2.Size = New System.Drawing.Size(58, 54)
+        Me.BtnC2.TabIndex = 5
+        Me.BtnC2.UseVisualStyleBackColor = False
+        '
+        'BtnA3
+        '
+        Me.BtnA3.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnA3.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnA3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnA3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnA3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnA3.Location = New System.Drawing.Point(13, 325)
+        Me.BtnA3.Name = "BtnA3"
+        Me.BtnA3.Size = New System.Drawing.Size(58, 54)
+        Me.BtnA3.TabIndex = 6
+        Me.BtnA3.UseVisualStyleBackColor = False
+        '
+        'BtnB3
+        '
+        Me.BtnB3.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnB3.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnB3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnB3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnB3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnB3.Location = New System.Drawing.Point(99, 325)
+        Me.BtnB3.Name = "BtnB3"
+        Me.BtnB3.Size = New System.Drawing.Size(58, 54)
+        Me.BtnB3.TabIndex = 7
+        Me.BtnB3.UseVisualStyleBackColor = False
+        '
+        'BtnC3
+        '
+        Me.BtnC3.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BtnC3.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnC3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aquamarine
+        Me.BtnC3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue
+        Me.BtnC3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray
+        Me.BtnC3.Location = New System.Drawing.Point(187, 325)
+        Me.BtnC3.Name = "BtnC3"
+        Me.BtnC3.Size = New System.Drawing.Size(58, 54)
+        Me.BtnC3.TabIndex = 8
+        Me.BtnC3.UseVisualStyleBackColor = False
+        '
+        'BtnNovoJogo
+        '
+        Me.BtnNovoJogo.BackColor = System.Drawing.Color.MediumSpringGreen
+        Me.BtnNovoJogo.FlatAppearance.BorderColor = System.Drawing.Color.Gray
+        Me.BtnNovoJogo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Teal
+        Me.BtnNovoJogo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnNovoJogo.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnNovoJogo.Location = New System.Drawing.Point(15, 404)
+        Me.BtnNovoJogo.Name = "BtnNovoJogo"
+        Me.BtnNovoJogo.Size = New System.Drawing.Size(232, 35)
+        Me.BtnNovoJogo.TabIndex = 9
+        Me.BtnNovoJogo.Text = "Novo Jogo"
+        Me.BtnNovoJogo.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft New Tai Lue", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(32, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 15)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "É a vez do jogador:"
+        '
+        'LblJogador
+        '
+        Me.LblJogador.AutoSize = True
+        Me.LblJogador.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LblJogador.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblJogador.Location = New System.Drawing.Point(35, 43)
+        Me.LblJogador.Name = "LblJogador"
+        Me.LblJogador.Size = New System.Drawing.Size(2, 33)
+        Me.LblJogador.TabIndex = 11
+        '
+        'BtnEditar
+        '
+        Me.BtnEditar.Image = CType(resources.GetObject("BtnEditar.Image"), System.Drawing.Image)
+        Me.BtnEditar.Location = New System.Drawing.Point(113, 88)
+        Me.BtnEditar.Name = "BtnEditar"
+        Me.BtnEditar.Size = New System.Drawing.Size(44, 39)
+        Me.BtnEditar.TabIndex = 12
+        Me.BtnEditar.UseVisualStyleBackColor = True
+        '
+        'Lbl1
+        '
+        Me.Lbl1.AutoSize = True
+        Me.Lbl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl1.Location = New System.Drawing.Point(10, 464)
+        Me.Lbl1.Name = "Lbl1"
+        Me.Lbl1.Size = New System.Drawing.Size(78, 15)
+        Me.Lbl1.TabIndex = 13
+        Me.Lbl1.Text = "JOGADOR 1 "
+        '
+        'LblVitoriasPlayer1
+        '
+        Me.LblVitoriasPlayer1.AutoSize = True
+        Me.LblVitoriasPlayer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblVitoriasPlayer1.Location = New System.Drawing.Point(15, 496)
+        Me.LblVitoriasPlayer1.Name = "LblVitoriasPlayer1"
+        Me.LblVitoriasPlayer1.Size = New System.Drawing.Size(15, 15)
+        Me.LblVitoriasPlayer1.TabIndex = 14
+        Me.LblVitoriasPlayer1.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(96, 464)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(75, 15)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "JOGADOR 2"
+        '
+        'LblVitoriasPlayer2
+        '
+        Me.LblVitoriasPlayer2.AutoSize = True
+        Me.LblVitoriasPlayer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblVitoriasPlayer2.Location = New System.Drawing.Point(99, 496)
+        Me.LblVitoriasPlayer2.Name = "LblVitoriasPlayer2"
+        Me.LblVitoriasPlayer2.Size = New System.Drawing.Size(15, 15)
+        Me.LblVitoriasPlayer2.TabIndex = 16
+        Me.LblVitoriasPlayer2.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(181, 464)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(64, 15)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "EMPATES"
+        '
+        'LblEmpates
+        '
+        Me.LblEmpates.AutoSize = True
+        Me.LblEmpates.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblEmpates.Location = New System.Drawing.Point(187, 496)
+        Me.LblEmpates.Name = "LblEmpates"
+        Me.LblEmpates.Size = New System.Drawing.Size(15, 15)
+        Me.LblEmpates.TabIndex = 18
+        Me.LblEmpates.Text = "0"
+        '
+        'JogodaVelha
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(279, 554)
+        Me.Controls.Add(Me.LblEmpates)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.LblVitoriasPlayer2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.LblVitoriasPlayer1)
+        Me.Controls.Add(Me.Lbl1)
+        Me.Controls.Add(Me.BtnEditar)
+        Me.Controls.Add(Me.LblJogador)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnNovoJogo)
+        Me.Controls.Add(Me.BtnC3)
+        Me.Controls.Add(Me.BtnB3)
+        Me.Controls.Add(Me.BtnA3)
+        Me.Controls.Add(Me.BtnC2)
+        Me.Controls.Add(Me.BtnB2)
+        Me.Controls.Add(Me.BtnA2)
+        Me.Controls.Add(Me.BtnC1)
+        Me.Controls.Add(Me.BtnB1)
+        Me.Controls.Add(Me.BtnA1)
+        Me.Name = "JogodaVelha"
+        Me.Text = "Jogo da Velha"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnA1 As Button
+    Friend WithEvents BtnB1 As Button
+    Friend WithEvents BtnC1 As Button
+    Friend WithEvents BtnA2 As Button
+    Friend WithEvents BtnB2 As Button
+    Friend WithEvents BtnC2 As Button
+    Friend WithEvents BtnA3 As Button
+    Friend WithEvents BtnB3 As Button
+    Friend WithEvents BtnC3 As Button
+    Friend WithEvents BtnNovoJogo As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LblJogador As Label
+    Friend WithEvents BtnEditar As Button
+    Friend WithEvents Lbl1 As Label
+    Friend WithEvents LblVitoriasPlayer1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents LblVitoriasPlayer2 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents LblEmpates As Label
+End Class
---
+++ jogo-da-velha/jogo-da-velha.vb
@@ -0,0 +1,195 @@
+﻿Public Class JogodaVelha
+
+    Dim player1, player2 As String
+    Dim jogadorDaVez As Integer
+
+    Private Sub BtnNovoJogo_Click(sender As Object, e As EventArgs) Handles BtnNovoJogo.Click
+
+        LblJogador.Text = player1
+        jogadorDaVez = 1
+
+        LblJogador.Text = player1
+        BtnA1.Text = ""
+        BtnA2.Text = ""
+        BtnA3.Text = ""
+        BtnB1.Text = ""
+        BtnB2.Text = ""
+        BtnB3.Text = ""
+        BtnC1.Text = ""
+        BtnC2.Text = ""
+        BtnC3.Text = ""
+
+
+        BtnA1.Enabled = True
+        BtnA2.Enabled = True
+        BtnA3.Enabled = True
+        BtnB1.Enabled = True
+        BtnB2.Enabled = True
+        BtnB3.Enabled = True
+        BtnC1.Enabled = True
+        BtnC2.Enabled = True
+        BtnC3.Enabled = True
+
+
+
+
+    End Sub
+
+    Private Sub BtnA1_Click(sender As Object, e As EventArgs) Handles BtnA1.Click
+
+        VerificarJogada(BtnA1)
+
+    End Sub
+
+    Private Sub BtnB1_Click(sender As Object, e As EventArgs) Handles BtnB1.Click
+
+        VerificarJogada(BtnB1)
+
+    End Sub
+
+    Private Sub BtnC1_Click(sender As Object, e As EventArgs) Handles BtnC1.Click
+
+        VerificarJogada(BtnC1)
+
+    End Sub
+
+    Private Sub BtnA2_Click(sender As Object, e As EventArgs) Handles BtnA2.Click
+
+        VerificarJogada(BtnA2)
+
+
+    End Sub
+
+    Private Sub BtnB2_Click(sender As Object, e As EventArgs) Handles BtnB2.Click
+
+        VerificarJogada(BtnB2)
+
+    End Sub
+
+    Private Sub BtnC2_Click(sender As Object, e As EventArgs) Handles BtnC2.Click
+
+        VerificarJogada(BtnC2)
+
+    End Sub
+
+    Private Sub BtnA3_Click(sender As Object, e As EventArgs) Handles BtnA3.Click
+
+        VerificarJogada(BtnA3)
+
+
+    End Sub
+
+    Private Sub BtnB3_Click(sender As Object, e As EventArgs) Handles BtnB3.Click
+
+        VerificarJogada(BtnB3)
+
+    End Sub
+
+    Private Sub BtnC3_Click(sender As Object, e As EventArgs) Handles BtnC3.Click
+        VerificarJogada(BtnC3)
+    End Sub
+
+    Private Sub VerificarJogada(botao As Button)
+        If (botao.Text = "") Then
+            If (jogadorDaVez = 1) Then
+                botao.Text = "X"
+                VerificarGanhador("X")
+                LblJogador.Text = player2
+                jogadorDaVez = 2
+            Else
+                botao.Text = "O"
+                VerificarGanhador("O")
+                LblJogador.Text = player1
+                jogadorDaVez = 1
+            End If
+
+
+
+        Else
+            MessageBox.Show("Opção já escolhida!")
+        End If
+    End Sub
+
+    Private Sub VerificarGanhador(simbolo As String)
+
+        Dim flagGanhador As Boolean = False
+
+        If (BtnA1.Text = simbolo And BtnA2.Text = simbolo And BtnA3.Text = simbolo) Then
+            flagGanhador = True
+        ElseIf (BtnB1.Text = simbolo And BtnB2.Text = simbolo And BtnB3.Text = simbolo) Then
+            flagGanhador = True
+
+        ElseIf (BtnC1.Text = simbolo And BtnC2.Text = simbolo And BtnC3.Text = simbolo) Then
+            flagGanhador = True
+
+        ElseIf (BtnA1.Text = simbolo And BtnB1.Text = simbolo And BtnC1.Text = simbolo) Then
+            flagGanhador = True
+
+        ElseIf (BtnA2.Text = simbolo And BtnB2.Text = simbolo And BtnC2.Text = simbolo) Then
+            flagGanhador = True
+
+        ElseIf (BtnA3.Text = simbolo And BtnB3.Text = simbolo And BtnC3.Text = simbolo) Then
+            flagGanhador = True
+
+        ElseIf (BtnA1.Text = simbolo And BtnB2.Text = simbolo And BtnC3.Text = simbolo) Then
+            flagGanhador = True
+
+        ElseIf (BtnC1.Text = simbolo And BtnB2.Text = simbolo And BtnA3.Text = simbolo) Then
+            flagGanhador = True
+        End If
+
+        If (flagGanhador = True) Then
+
+            MessageBox.Show(LblJogador.Text & "   GANHOUU!!!")
+            If (jogadorDaVez = 1) Then
+                LblVitoriasPlayer1.Text = LblVitoriasPlayer1.Text + 1
+            Else
+                LblVitoriasPlayer2.Text = LblVitoriasPlayer2.Text + 1
+            End If
+
+
+            BtnA1.Enabled = False
+            BtnA2.Enabled = False
+            BtnA3.Enabled = False
+            BtnB1.Enabled = False
+            BtnB2.Enabled = False
+            BtnB3.Enabled = False
+            BtnC1.Enabled = False
+            BtnC2.Enabled = False
+            BtnC3.Enabled = False
+
+        Else
+            If (BtnA1.Text <> "" And BtnA2.Text <> "" And BtnA3.Text <> "" And BtnB1.Text <> "" And BtnB2.Text <> "" And BtnB3.Text <> "" And BtnC1.Text <> "" And BtnC2.Text <> "" And BtnC3.Text <> "") Then
+                MessageBox.Show("EMPATE!!")
+                LblEmpates.Text = LblEmpates.Text + 1
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub BtnEditar_Click(sender As Object, e As EventArgs) Handles BtnEditar.Click
+
+        If (jogadorDaVez = 1) Then
+            player1 = InputBox("Digite o nome do JOGADOR 1 ").ToUpper
+            LblJogador.Text = player1
+
+        Else
+            player2 = InputBox("Digite o nome do JOGADOR 2 ").ToUpper
+            LblJogador.Text = player2
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        player1 = InputBox("Digite o nome do JOGADOR 1").ToUpper
+        player2 = InputBox("Digite o nome do JOGADOR 2 ").ToUpper
+
+        'JogodaVelha_Load
+
+        LblJogador.Text = player1
+        jogadorDaVez = 1
+    End Sub
+End Class
